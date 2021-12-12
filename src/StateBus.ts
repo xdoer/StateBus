edbab87396c872b79fd3ad4b3cafa653803d0fce
xdoer <gotoanything@foxmail.com>
2021-12-12 14:59:15 +0800

    fix: getState return a function

--- src/StateBus.ts
+++ src/StateBus.ts
@@ -3,8 +3,11 @@ import React, { useState, useEffect } from 'react'
 type Dispatch<T> = React.Dispatch<React.SetStateAction<T>>
 
 export class StateBus<T = any> {
+  private stateStack = []
 
-  constructor(private state: T | (() => T)) { }
+  constructor(private state: T | (() => T)) {
+    this.stateStack.push(state)
+  }
 
   private listeners: Dispatch<T>[] = []
 
@@ -17,24 +20,31 @@ export class StateBus<T = any> {
     if (idx !== -1) this.listeners.splice(idx, 1)
   }
 
-  getState() {
-    return this.state
+  getState(): T {
+    if (typeof this.state !== 'function') return this.state
+
+    this.state = this.stateStack.reduce((result, current) => typeof current === 'function' ? current(result) : current, undefined)
+
+    return this.state as T
   }
 
-  setState(data: T | ((prevState: T) => T)) {
+  setState(data: T | ((prevState?: T) => T)) {
     this.listeners.forEach(listener => listener(data))
+
+    this.stateStack.push(data)
+    if (typeof data !== 'function') this.state = data
   }
 
   useState() {
     const [data, setData] = useState<T>(this.state)
-
-    if (this.state !== data) this.state = data
 
     useEffect(() => {
       this.subscribe(setData)
 
       return () => {
         this.unsubscribe(setData)
+
+        if (!this.listeners.length) this.stateStack = []
       }
     }, [])
 
